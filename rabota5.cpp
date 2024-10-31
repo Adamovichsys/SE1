387bc7fb47c3d7868876e7eaa0a423ec77e6b437
Dmitriy Adamovich <Dimasik1337@gmail.com>
2024-10-31 20:36:00 +0300

    summ of a+b

---
+++ rabota5.cpp
@@ -0,0 +1,14 @@
+//SOME FEATURE
+//This is example program
+// Authors: Adamovich KE-317
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<< "enter numbers a,b"; cin>>a>>b;
+    cout<<"sum of a*b="<<a*b<<endl;
+    return 0;
+}
